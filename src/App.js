41eff79d49cdf23209239bb0b02b6964dffc8786
unknown <krishnanapm13@gmail.com>
2023-03-13 15:43:21 +0530

    first day

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Hello World </h1>
+      <button>Click me</button>
+    </div>
+  );
+}
+
+export default App;
